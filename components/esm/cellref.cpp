fb45888009814bac00d455b1216bd7710c4a965a
skooma-breath <latencyslinger@gmail.com>
2026-01-19 10:19:08 -0600

    unclamp scale from 2....

--- components/esm/cellref.cpp
+++ components/esm/cellref.cpp
@@ -72,10 +72,12 @@ void ESM::CellRef::loadData(ESMReader &esm, bool &isDeleted)
                 break;
             case ESM::FourCC<'X','S','C','L'>::value:
                 esm.getHT(mScale);
-                if (mScale < 0.5)
+                /*if (mScale < 0.5)
                     mScale = 0.5;
                 else if (mScale > 2)
-                    mScale = 2;
+                    mScale = 2;*/
+                if (mRefID == "caius cosades")
+                    std::cout << "DEBUG: Loaded scale value: " << mScale << " for " << mRefID << std::endl;
                 break;
             case ESM::FourCC<'A','N','A','M'>::value:
                 mOwner = esm.getHString();
@@ -153,12 +155,13 @@ void ESM::CellRef::save (ESMWriter &esm, bool wideRefNum, bool inInventory, bool
     }
 
     if (mScale != 1.0) {
-        float scale = mScale;
-        if (scale < 0.5)
+        //float scale = mScale;
+       /* if (scale < 0.5)
             scale = 0.5;
         else if (scale > 2)
-            scale = 2;
-        esm.writeHNT("XSCL", scale);
+            scale = 2;*/
+        //esm.writeHNT("XSCL", scale);
+        esm.writeHNT("XSCL", mScale);
     }
 
     if (!inInventory)
